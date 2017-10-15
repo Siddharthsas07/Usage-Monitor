7f5a4b8fa8eb6b24ce76cdc8f3021f022e308e84
Siddharth Shah <shahsiddharth.10@gmail.com>
2017-10-15 14:51:06 -0500

    endpoint - adding api/usage endpoint

---
+++ controllers/apiController.js
@@ -0,0 +1,67 @@
+var bodyParser = require('body-parser');
+var os = require('os');
+
+module.exports = function(app) {
+
+    // bodyParser will help with parsing data from URL
+    // it is used for many things including pattern matching
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended : true}));
+
+
+    function getCPUInfo(){ 
+        var cpus = os.cpus();
+        
+        var user = 0;
+        var nice = 0;
+        var sys = 0;
+        var idle = 0;
+        var irq = 0;
+        var total = 0;
+        
+        for(var cpu in cpus){
+            if (!cpus.hasOwnProperty(cpu)) continue;	
+            user += cpus[cpu].times.user;
+            nice += cpus[cpu].times.nice;
+            sys += cpus[cpu].times.sys;
+            irq += cpus[cpu].times.irq;
+            idle += cpus[cpu].times.idle;
+        }
+        
+        var total = user + nice + sys + idle + irq;
+        
+        return {
+            'idle': idle, 
+            'total': total
+        };
+    }
+
+    app.get('/api/usage', function(req, res) {
+        // http response
+        
+        // console.log(os.cpus());
+        console.log(os.totalmem());
+        console.log(os.freemem());
+
+        var stats1 = getCPUInfo();
+        var startIdle = stats1.idle;
+        var startTotal = stats1.total;
+        
+        setTimeout(function() {
+            var stats2 = getCPUInfo();
+            var endIdle = stats2.idle;
+            var endTotal = stats2.total;
+            
+            var idle 	= endIdle - startIdle;
+            var total 	= endTotal - startTotal;
+            var perc	= idle / total;
+              
+            console.log((1 - perc) * 100 + '%');
+                  
+        }, 1000 );
+
+
+        // just sending string of todos back. Will be in JSON format as mongoDb stores it as documents of JSONs
+        res.send({'usage' : 1 - perc});
+    });
+}
